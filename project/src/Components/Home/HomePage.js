abe80338f3359663718ca6fae9336333c9eff315
rking12 <48954882+rking12@users.noreply.github.com>
2020-08-12 02:10:08 +0530

    ok

---
+++ project/src/Components/Home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homestyle.css'
+
+function HomePage() {
+    return (
+      <div className="mainhome">
+          dsfa
+
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
